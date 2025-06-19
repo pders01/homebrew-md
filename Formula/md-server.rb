f3a9508bf79fa600099bcbf38e9047653388bbd9
pders01 <paulderscheid@gmail.com>
2025-06-20 01:10:37 +0200

    feat: update formula to use bundled CLI v1.0.4
    - Update to use @jpahd/md-server v1.0.4
    
    - Fix version detection for Homebrew installations
    
    - Install static assets and package.json to libexec
    
    - Update SHA256 hash for new tarball

--- Formula/md-server.rb
+++ Formula/md-server.rb
@@ -1,8 +1,8 @@
 class MdServer < Formula
   desc "A simple CLI tool that serves markdown files as HTML with beautiful Tailwind CSS styling"
   homepage "https://github.com/pders01/md"
-  url "https://registry.npmjs.org/@jpahd/md-server/-/md-server-1.0.1.tgz"
-  sha256 "ca98d622480b031103cedf42bfa17fbd386a780f035618774746c1ec942a4c2b"
+  url "https://registry.npmjs.org/@jpahd/md-server/-/md-server-1.0.4.tgz"
+  sha256 "4f2f2cc001f97fba55a1cdbcca4dd0f628751e8a8d0f133fe39de8e9c037fa49"
   license "MIT"
 
   depends_on "node"
@@ -10,11 +10,17 @@ class MdServer < Formula
   def install
     system "tar", "-xzf", cached_download
     Dir.chdir("package") do
+      # Install the CLI binary
       bin.install "dist/index.js" => "md-server"
+      
+      # Install static assets and template
+      libexec.install "dist/public"
+      libexec.install "dist/template.html"
+      libexec.install "dist/package.json"
     end
   end
 
   test do
     system "#{bin}/md-server", "--help"
   end
-end
+end 
