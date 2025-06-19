5f0e1f3722344c8155f7923349d396a1ac917fe4
pders01 <paulderscheid@gmail.com>
2025-06-20 00:28:53 +0200

    Fix npm installation method to avoid Language::Node dependency

--- Formula/md-server.rb
+++ Formula/md-server.rb
@@ -8,8 +8,8 @@ class MdServer < Formula
   depends_on "node"
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
-    bin.install_symlink libexec/"bin/md-server"
+    system "npm", "install", "-g", "@jpahd/md-server"
+    bin.install_symlink Dir[HOMEBREW_PREFIX/"lib/node_modules/@jpahd/md-server/cli.js"] => "md-server"
   end
 
   test do
