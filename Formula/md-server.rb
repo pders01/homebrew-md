f8512635d3db64a0cd7f6795584091144abb6abd
pders01 <paulderscheid@gmail.com>
2025-06-20 00:20:05 +0200

    Install md-server via npm package @jpahd/md-server

--- Formula/md-server.rb
+++ Formula/md-server.rb
@@ -1,19 +1,18 @@
 class MdServer < Formula
   desc "A simple CLI tool that serves markdown files as HTML with beautiful Tailwind CSS styling"
   homepage "https://github.com/pders01/md"
-  url "https://github.com/pders01/md.git", branch: "main"
   version "1.0.0"
   license "MIT"
 
   depends_on "node"
 
   def install
-    system "npm", "install"
-    system "npm", "run", "build"
-    bin.install "dist/md-server.js" => "md-server"
+    system "npm", "install", "-g", "@jpahd/md-server"
+    # Symlink the installed binary into Homebrew's bin
+    bin.install_symlink Dir[HOMEBREW_PREFIX/"lib/node_modules/@jpahd/md-server/cli.js"] => "md-server"
   end
 
   test do
     system "#{bin}/md-server", "--help"
   end
-end 
+end
