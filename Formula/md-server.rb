3ca5209a47a8fbe84a3614694964220d99269d21
pders01 <paulderscheid@gmail.com>
2025-07-05 11:15:25 +0200

    feat: update formula to use bundled CLI v1.1.2
    - Update to use @jpahd/md-server v1.1.2
    - Cleanup of AI and human slop
    - Update SHA256 hash for new tarball

--- Formula/md-server.rb
+++ Formula/md-server.rb
@@ -1,8 +1,8 @@
 class MdServer < Formula
   desc "A simple CLI tool that serves markdown files as HTML with beautiful Tailwind CSS styling"
   homepage "https://github.com/pders01/md"
-  url "https://registry.npmjs.org/@jpahd/md-server/-/md-server-1.0.4.tgz"
-  sha256 "4f2f2cc001f97fba55a1cdbcca4dd0f628751e8a8d0f133fe39de8e9c037fa49"
+  url "https://registry.npmjs.org/@jpahd/md-server/-/md-server-1.1.2.tgz"
+  sha256 "2ae29b74bda6af539c34cc145fea014e2c10de538454d0d3bc8456e0a4ad10a5"
   license "MIT"
 
   depends_on "node"
